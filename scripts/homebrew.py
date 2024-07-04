c5dc41801d58f8886559173ee972d244fcd31201
PoloNX <geyrecharlie@gmail.com>
2024-07-04 22:08:44 +0200

    added homebrews and sysmodules

--- scripts/homebrew.py
+++ scripts/homebrew.py
@@ -16,14 +16,22 @@ class Homebrew(BaseModule):
         a.append([{"username": "PoloNX","reponame": "Ls-News","assetPatterns": [".*Ls-News.*\\.nro"]}])
         a.append([{"username": "PoloNX","reponame": "SimpleModDownloader","assetPatterns": [".*SimpleModDownloader.*\\.nro"]}])
         a.append([{"username": "nadrino","reponame": "SimpleModManager","assetPatterns": [".*SimpleModManager.*\\.nro"]}])
-        a.append([{"username": "znxDomain","reponame": "DNS-MITM_Manager","assetPatterns": [".*DNS-MITM_Manager.*\\.nro"]}])
-
+        a.append([{"username": "znxDomain","reponame": "DNS-MITM_Manager","assetPatterns": [".*DNS-MITM_Manager.*\\.zip"]}])
 
         for i in a:
             self.config = i
-            if self.config[0]["reponame"] == "emuiibo":
-                release = self.get_latest_pre_release(0)
+            release = self.get_latest_release(0)
+            
+            # Vérifier si assetPatterns est défini et non vide
+            if "assetPatterns" in self.config[0] and self.config[0]["assetPatterns"]:
+                asset = self.get_asset_link(release, self.config[0]["assetPatterns"][0])
+                self.out[self.config[0]["reponame"]] = {
+                    "name": i[0]["reponame"],
+                    "link": asset[0].browser_download_url,
+                    "version": release.tag_name
+                }
             else:
-                release = self.get_latest_release(0)
-            asset = self.get_asset_link(release, self.config[0]["assetPatterns"][0])
-            self.out[self.config[0]["reponame"]] = {"name": i[0]["reponame"] , "link": asset[0].browser_download_url, "version": release.tag_name}
+                # Gérer le cas où assetPatterns est vide ou non défini
+                print(f"Warning: No assetPatterns defined for repo {self.config[0]['reponame']}")
+
+
--- scripts/sysmodule.py
+++ scripts/sysmodule.py
@@ -12,7 +12,7 @@ class Sysmodule(BaseModule):
         a.append([{"username": "WerWolv","reponame": "nx-ovlloader","assetPatterns": [".*nx-ovlloader.*\\.zip"], "archiveDirectory": "/", "archiveExtraction": "sdmc:/"}])
         a.append([{"username": "HookedBehemoth","reponame": "sys-tune","assetPatterns": [".*sys-tune.*\\.zip"], "archiveDirectory": "/", "archiveExtraction": "sdmc:/"}])
         a.append([{"username": "XorTroll","reponame": "emuiibo","assetPatterns": [".*emuiibo.*\\.zip"], "archiveDirectory": "/SdOut/", "archiveExtraction": "sdmc:/"}])
-        a.append([{"username": "o0Zz","reponame": "sys-con","assetPatterns": [".*sys-con.*\\1.7.x.zip"], "archiveDirectory": "/", "archiveExtraction": "sdmc:/"}])
+        #a.append([{"username": "o0Zz","reponame": "sys-con","assetPatterns": [".*sys-con.*\\1.7.x.zip"], "archiveDirectory": "/", "archiveExtraction": "sdmc:/"}])
         a.append([{"username": "retronx-team","reponame": "sys-clk","assetPatterns": [".*sys-clk.*\\.zip"], "archiveDirectory": "/", "archiveExtraction": "sdmc:/"}])
         a.append([{"username": "bakatrouble","reponame": "sys-screenuploader","assetPatterns": [".*sys-screenuploader.*\\.zip"], "archiveDirectory": "/", "archiveExtraction": "sdmc:/"}])
         #a.append([{"username": "impeeza","reponame": "sys-patch","assetPatterns": [".*sys-patch.*\\.zip"], "archiveDirectory": "/", "archiveExtraction": "sdmc:/"}])
@@ -21,5 +21,18 @@ class Sysmodule(BaseModule):
         for i in a:
             self.config = i
             release = self.get_latest_release(0)
-            asset = self.get_asset_link(release, self.config[0]["assetPatterns"][0])
-            self.out[self.config[0]["reponame"]] = {"name": i[0]["reponame"] , "link": asset[0].browser_download_url, "version": release.tag_name}
+            
+            # Vérifier si assetPatterns est défini et non vide
+            if "assetPatterns" in self.config[0] and self.config[0]["assetPatterns"]:
+                asset = self.get_asset_link(release, self.config[0]["assetPatterns"][0])
+                self.out[self.config[0]["reponame"]] = {
+                    "name": i[0]["reponame"],
+                    "link": asset[0].browser_download_url,
+                    "version": release.tag_name,
+                    "archiveDirectory": i[0]["archiveDirectory"],
+                    "archiveExtraction": i[0]["archiveExtraction"]
+                }
+            else:
+                # Gérer le cas où assetPatterns est vide ou non défini
+                print(f"Warning: No assetPatterns defined for repo {self.config[0]['reponame']}")
+
